449e042026d447cc1ad506822c9aedaaad5edd2e
brilliantdrink <166573010+brilliantdrink@users.noreply.github.com>
2024-08-12 15:32:28 -0700

    wasTooGood: check for actual win

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,52 @@ import ModeSwitch from './components/ModeSwitch'
 import HardModeDialog from './components/HardModeDialog'
 import createPersistedSignal from './persistedSignal'
 import {makeSubmitDebouncedHandler} from './submit-guess'
-import {dateNumber, randomForDate} from './pseudo-random'
+import {dateNumber, randomForDate, randomForToday} from './pseudo-random'
 
 import clipsDb from '../clips-db.txt'
 
-const previousGuesses: number[][] = []
-for (var i = 0, len = localStorage.length; i < len; ++i) {
-  const key = localStorage.key(i)
-  if (!key || !key.endsWith('guesses') || key.startsWith(String(dateNumber))) continue
-  previousGuesses.push(JSON.parse(localStorage.getItem(key) as string))
+function isValidDate(d: Date) {
+  return !isNaN(d.valueOf())
 }
 
-const wasTooGood = previousGuesses.filter(guess => guess.length <= 2).length >= 3
+let setWasTooGood: (value: boolean) => void
+let wasTooGoodPromise = new Promise<boolean>(res => setWasTooGood = res)
+
+let previousCorrectGuessesAmount = 0
+
+let setClips: (value: ([string, string, string, string] | null)[]) => void
+const clips = new Promise<([string, string, string, string] | null)[]>((res) => setClips = res)
+
+const digitRegex = /\d+/
+const clipPromises: Promise<void>[] = []
+
+// todo remove duplicate code into functions
+for (let i = 0, len = localStorage.length; i < len; ++i) {
+  const key = localStorage.key(i)
+  if (!key || !key.endsWith('guesses') || key.startsWith(String(dateNumber))) continue
+  const guess = JSON.parse(localStorage.getItem(key) as string)
+  if (guess.length > 2) continue
+  const thenDateNumberVal = (key.match(digitRegex) as RegExpMatchArray)[0]
+  let thenDateNumberString = String(Number(thenDateNumberVal) / 200)
+  // this only happened for 8.8.2024 - 12.8.2024
+  if (thenDateNumberString.length === 6) thenDateNumberString = '0' + thenDateNumberString
+  if (thenDateNumberString.length === 7) thenDateNumberString = thenDateNumberString.substring(0, 2) + '0' + thenDateNumberString.substring(2)
+  const thenDate = new Date(Number(thenDateNumberString.substring(4)), Number(thenDateNumberString.substring(2, 4)), Number(thenDateNumberString.substring(0, 2)))
+  const hardMode = localStorage.getItem(thenDateNumberVal + '_hard_mode') ?? false
+  clipPromises.push(clips.then(clips => {
+    let clip = null, j = 0
+    // console.log(clips[Math.floor(randomForDate(thenDate, j++, false) * clips.length)])
+    do clip = clips[Math.floor(randomForDate(thenDate, j++, false) * clips.length)]; while (!clip)
+    const lastGuessOfDate = (!hardMode ? steps : stepsFine)[guess.at(-1)]
+    const dateStr = clip[1]
+    if (!dateStr) return undefined
+    const clipDate = [dateStr.substring(0, 4), dateStr.substring(5, 7), dateStr.substring(8, 10)].map(Number) as [number, number, number]
+    const guessCorrect = isNotAfter(lastGuessOfDate.startRange, clipDate) &&
+      isNotAfter(clipDate, lastGuessOfDate.endRange)
+    if (guessCorrect) previousCorrectGuessesAmount++
+  }))
+}
+Promise.all(clipPromises).then(() => setWasTooGood(previousCorrectGuessesAmount >= 3))
 
 export default function App() {
   const [guesses, setGuesses] = createPersistedSignal<number[]>(String(dateNumber) + '_guesses', [])
@@ -37,13 +71,22 @@ export default function App() {
   const helpDialogInitialOpen = createMemo(() => !localStorage.getItem('seen_help'))
 
   const [allClips, setAllClips] = createSignal<([string, string, string, string] | null)[]>(null!)
+  const [wasTooGood, setWasTooGood] = createSignal<boolean>(null!)
 
   createEffect(() => {
     if (allClips()) return
     fetch(clipsDb).then(res => res.text())
       .then(text =>
         text.split('\n').map(line => line.startsWith('!') ? null : line.split(' ')) as ([string, string, string, string] | null)[]
-      ).then(setAllClips)
+      )
+      .then(clips => {
+        setAllClips(clips)
+        setClips(clips)
+      })
+  })
+  createEffect(() => {
+    if (typeof wasTooGood() === 'boolean') return
+    wasTooGoodPromise.then(setWasTooGood)
   })
 
   createEffect(() => {
@@ -72,7 +115,7 @@ export default function App() {
   const clipData = createMemo(() => {
     if (!allClips()) return null
     let clip = null, i = 0
-    do clip = allClips()[Math.floor(randomForDate(i++) * allClips()?.length)]; while (!clip)
+    do clip = allClips()[Math.floor(randomForToday(i++) * allClips()?.length)]; while (!clip)
     return clip
   })
 
@@ -93,16 +136,24 @@ export default function App() {
   })
 
   const handleSubmitDebounce =
-    makeSubmitDebouncedHandler({guesses, setGuesses, slideValue, clipDate, setGameEnded, steps: stepsAdaptive, hardMode})
+    makeSubmitDebouncedHandler({
+      guesses,
+      setGuesses,
+      slideValue,
+      clipDate,
+      setGameEnded,
+      steps: stepsAdaptive,
+      hardMode
+    })
 
   return <>
-    {wasTooGood && <HardModeDialog/>}
+    {wasTooGood() && <HardModeDialog />}
     <Toaster position={'top-center'} gutter={8} />
     <Header helpDialogInitialOpen={helpDialogInitialOpen()} />
     <TwitchEmbed id={clipId} />
     <div class={styles.inputs}>
       {guesses().length === 0
-        ? wasTooGood && <ModeSwitch hardMode={hardMode} setHardMode={setHardMode} />
+        ? wasTooGood() && <ModeSwitch hardMode={hardMode} setHardMode={setHardMode} />
         : <Guesses guesses={guesses} clipDate={clipDate} gameEnded={gameEnded} won={won} hardMode={hardMode} />
       }
       {hardMode() && <p class={styles.monthHint}>(early: 1 - 10, mid: 11 - 20, late: 21 - 31)</p>}
--- src/pseudo-random.ts
+++ src/pseudo-random.ts
@@ -8,7 +8,16 @@ const c = 1013904223 // Increment
 const m = 2 ** 32 // Modulus
 
 const currentDate = new Date()
-currentDate.setDate(currentDate.getDate() + 5)
-export const dateNumber = Number(String(currentDate.getDate()) + String(currentDate.getMonth()) + String(currentDate.getFullYear())) * 200
+currentDate.setDate(currentDate.getDate() + 5) // why tf did I do this????
+export const dateNumber = Number(
+  String(currentDate.getDate()).padStart(2, '0') +
+  String(currentDate.getMonth()).padStart(2, '0') +
+  String(currentDate.getFullYear())
+) * 200
 
-export const randomForDate = (iter: number) => lcg(a, c, m, Number(iter + String(dateNumber)))
+export const randomForToday = (iter: number) => lcg(a, c, m, Number(iter + String(dateNumber)))
+export const randomForDate = (date: Date, iter: number, shift: boolean = true) => {
+  if (shift) date.setDate(date.getDate() + 5)
+  const dateNumber = Number(String(date.getDate()) + String(date.getMonth()) + String(date.getFullYear())) * 200
+  return lcg(a, c, m, Number(iter + String(dateNumber)))
+}
